d40fe0ed931e3171a337339f93f3180330c3c619
Brent Yi <yibrenth@gmail.com>
2017-11-17 15:03:48 -0800

    unexecutable

--- src/mouth_detector.py
+++ src/mouth_detector.py
@@ -1,30 +1,41 @@
 import cv2
 import numpy as np
+from sensor_msgs.msg import Image
 
-mouth_cascade = cv2.CascadeClassifier('./haarcascade_mcs_mouth.xml')
+class MouthDetector:
 
-if mouth_cascade.empty():
-    raise IOError('Unable to load the mouth cascade classifier xml file')
+    def __init__(self):
+        self.mouth_cascade = cv2.CascadeClassifier('./haarcascade_mcs_mouth.xml')
+        self.ds_factor = 0.5
 
-cap = cv2.VideoCapture(0)
-ds_factor = 0.5
+        if self.mouth_cascade.empty():
+            raise IOError('Unable to load the mouth cascade classifier xml file')
+        rospy.init_node('mouth_detector')
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
-    for (x,y,w,h) in mouth_rects:
-        y = int(y - 0.15*h)
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
-        break
+        rospy.Subscriber("/usb_cam/image_raw", Image, self.imageReceived)
 
-    cv2.imshow('Mouth Detector', frame)
 
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
+    def imageReceived(self, image):
+        self.frame = image
 
-cap.release()
-cv2.destroyAllWindows()
+    def run(self):
+        self.frame = cv2.resize(self.frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+        mouth_rects = self.mouth_cascade.detectMultiScale(gray, 1.7, 11)
+        for (x,y,w,h) in mouth_rects:
+            y = int(y - 0.15*h)
+            cv2.rectangle(self.frame, (x,y), (x+w,y+h), (0,255,0), 3)
+            break
+
+        cv2.imshow('Mouth Detector', self.frame)
+
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+        rospy.spin()
+
+if __name__ == '__main__':
+    node = MouthDetector()
+    node.run()
