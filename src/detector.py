9869d3a33e01717239b3b2731d17b939a746e634
Joshua Yuan <joshua43210@gmail.com>
2017-11-22 03:46:40 -0800

    cascade file wont load

--- src/detector.py
+++ src/detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
+import time
 
 
 class Detector:
@@ -9,34 +10,58 @@ class Detector:
             self.x = x # int
             self.y = y # int
     class Rectangle:
-        def __init__(self, top_left=Point(), width=0, height=0):
+        def __init__(self, top_left=None, width=0, height=0):
             self.top_left = top_left # Point()
             self.width = width # int
             self.height = height # int
+    class Timer:
+        def __init(self):
+            self.start = 0
+            self.runnig = False
 
     SCALE_FACTOR = 0.5
     def __init__(self):
-        self.face_pos = Point()
+        self.face_pos = self.Point()
+        self.face = self.Rectangle()
         self.frame = None
         self.found_face = False
         self.faces = []
-        self.currently_template_matching = False
+        self.face_roi = self.Rectangle()
+        self.face_template = None # a np matrix
+        self.match_result = None # a np matrix i think
+        self.timer = self.Timer()
+
+        self.face_cascade = None
+        self.i = 1
+
+    def loadCascade(self, path):
+        self.face_cascade = cv2.CascadeClassifier(path)
+        print(self.face_cascade.load(path))
+        print(self.face_cascade)
+        print(path)
+
+    def hasEmptyCascade(self):
+        return self.face_cascade.empty()
 
     def loadFrameFindFace(self, frame):
         self.frame = frame
+        if self.i == 1:
+            print(self.frame.shape)
+            print(self.frame)
+            self.i = 2
+
         
         self.frame = cv2.resize(self.frame, None, fx=self.SCALE_FACTOR, fy=self.SCALE_FACTOR, interpolation=cv2.INTER_AREA)
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-
         if not self.found_face:
             self.detect_all_faces(self.frame)
         else:
             self.detect_face_in_roi(self.frame)
-            if self.currently_template_matching:
+            if self.timer.running:
                 self.detect_face_with_template_matching(self.frame)
         return self.face_pos
     
-    def doubled_rectangle(self, input_rectangle, frame_size):
+    def double_this_rectangle(self, input_rectangle, frame_size):
         """
         input_rectangle is a Rectangle()
         frame_size is a tuple (frame width, frame height)
@@ -45,7 +70,7 @@ class Detector:
         if the new rectangle is out of bounds, we maintain the width and height and
         bring it into the frame space.
         """
-        new_rect = Rectangle()
+        new_rect = self.Rectangle()
         new_rect.width = input_rectangle.width * 2
         new_rect.height = input_rectangle.height * 2
         new_rect.top_left.x = max(0, input_rectangle.top_left.x - input_rectangle.width / 2)
@@ -60,37 +85,36 @@ class Detector:
 
     def find_biggest_face(self, faces):
         """
-        faces is an array of numpy matrices (a matrix of pixels)
+        faces is an array of rectangles of the format (x, y, w, h).
 
-        returns the face matrix with the biggest area = width * height
+        returns the Rectangle() with the biggest area.
         """
         biggest_face = (None, 0)
-        for f in faces:
-            w, h = f.shape
+        for x, y, w, h in faces:
             if biggest_face[1] < w*h:
-                biggest_face = (f, w*h)
+                biggest_face = (self.Rectangle(self.Point(x, y), w, h), w*h)
         return biggest_face[0]
 
     def get_rectangle_center(self, rectangle):
         """
         rectangle is a Rectangle()
 
-        returns the center Point()
+        returns the center Point() of that rectangle
         """
-        return Point(rectangle.top_left.x + rectangle.width / 2, \
+        return self.Point(rectangle.top_left.x + rectangle.width / 2, \
                      rectangle.top_left.y + rectangle.height / 2)
 
     
     def get_face_template(self, frame, current_face):
         """
-        frame is the global frame
-        current_face is a Rectangle() describing the face we're considering
+        frame is the global frame, a numpy matrix
+        current_face is a Rectangle() describing the area of the frame we're considering
 
-        returns the face's template matrix, which is the small patch rectangle
+        returns the face template as a np matrix, which is the small patch rectangle
         in the middle of the face
         """
-        new_face_rect = Rectangle()
-        new_face_rect.top_left = Point(current_face.top_left.x + current_face.width /4, current_face.top_left.y + current_face.height / 4)
+        new_face_rect = self.Rectangle()
+        new_face_rect.top_left = self.Point(current_face.top_left.x + current_face.width /4, current_face.top_left.y + current_face.height / 4)
         new_face_rect.width = current_face.width / 2
         new_face_rect.height = current_face.height / 2
 
@@ -98,5 +122,63 @@ class Detector:
                      new_face_rect.top_left.y:new_face_rect.top_left.y + new_face_rect.height]
 
     def detect_all_faces(self, frame):
+        self.faces = self.face_cascade.detectMultiScale(self.frame, 1.5, 10) #Add min & max sizing later
+        if not self.faces:
+            print("no faces")
+            return
+
+        print("found a face")
+        self.found_face = True
+        self.face = self.find_biggest_face(self.faces)
+        self.face_template = self.get_face_template(self.frame, self.face)
+        self.face_roi = self.double_this_rectangle(self.face, self.frame.shape)
+        self.face_pos = self.get_rectangle_center(self, self.face)
+
+    def detect_faces_around_roi(self, frame):
+        self.faces = self.face_cascade.detectMultiScale(self.frame, 1.1, 3) #min and max should be +/- 20%
+        if not self.faces:
+            self.timer.running = True
+            if self.timer.start == 0:
+                self.timer.start = time.time()
+            return
+        self.timer.running = False
+        self.timestamp.start = 0
         
+        self.face = self.find_biggest_face(self.faces)
+
+        self.face_template = self.get_face_template(self.frame, self.face)
+        self.face_roi = self.double_rectangle(self.face, self.frame.shape)
+        self.face_pos = self.get_rectangle_center(self.face)
+
+    def detect_faces_with_template_matching(self, frame):
+        if time.time() - self.timer.start > 2:
+            self.found_face = False
+            self.timer.running = False
+            self.timer.start = 0
+            self.face_pos = self.Point()
+            self.face = self.Rectangle()
+            return
+        self.match_result = cv2.matchTemplate(frame, self.face_template, cv2.CV_TM_SQDIFF_NORMED)
+        cv2.normalize(self.match_result, match_result, 0, 1, cv2.NORM_MINMAX)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(self.match_result)
+        top_left = max_loc
+        bottom_right = (top_left[0] + self.face_template.shape[0], top_left[1] + self.face_template.shape[1])
+        
+        self.face = self.Rectangle(self.Point(min_loc[0], min_loc[1]), self.face_template.shape[0], self.face_template.shape[1])
+        self.face = self.double_rectangle(self.face, self.frame.shape)
+
+        self.face_template = self.get_face_template(self.frame, self.face)
+        self.face_roi = self.double_rectangle(self.face, self.frame.shape)
+        self.face_pos = self.get_rectangle_center(self.face)
+    def faceIsFound(self):
+        return self.found_face
+    def getFacePosition(self):
+        return self.face_pos
+    def getFaceBox(self):
+        return (self.face.top_left.x, self.face.top_left.y, self.face.top_left.x + self.face.width, self.face.top_left.y + self.face.height)
+
+
+
+
+
 
