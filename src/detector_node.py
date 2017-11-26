25ad178d06663c60a3ec260020605eff0e03fc5b
Joshua Yuan <joshua43210@gmail.com>
2017-11-26 03:10:35 +0000

    dog

--- src/detector_node.py
+++ src/detector_node.py
@@ -7,24 +7,13 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
 class Node:
-<<<<<<< HEAD
 
-=======
     SCALE_FACTOR = 0.5
->>>>>>> edbd127b8a02a424e5b2e4d20a488e92b5e1482f
     def __init__(self):
         self.bridge = CvBridge()
         self.detector = detector.Detector()
         print("PLEASE")
-<<<<<<< HEAD
-        self.detector.loadCascade('/home/cc/ee106a/fa17/class/ee106a-aec/marshmello/src/mouth-detector/src/haarcascade_frontalface_default.xml')
-        if self.detector.hasEmptyCascade():
-            raise IOError('Unable to load the face cascade classifier xml file')
 
-
-        rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
-
-=======
         cascade_path = rospy.getparam('cascade_path')
         self.detector.loadCascade(cascade_path)
         if self.detector.hasEmptyCascade():
@@ -36,19 +25,12 @@ class Node:
         self.basic_frame = None
         self.previous_coords = ((0, 0),(1, 1)) # (x, y) of top left
 
->>>>>>> edbd127b8a02a424e5b2e4d20a488e92b5e1482f
     def callback(self, image):
         try:
             self.frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
             print(e)
         self.detector.loadFrameFindFace(self.frame)
-<<<<<<< HEAD
-        if self.detector.faceIsFound():
-            x1, y1, x2, y2 = self.detector.getFaceBox()
-            print(x1, y1, x2, y2)
-            cv2.rectangle(self.frame, (x1, y1), (x2, t2), (0, 255, 0), 2)
-=======
 
         self.basic_frame = cv2.resize(self.basic_frame, None, fx=self.SCALE_FACTOR, fy=self.SCALE_FACTOR, interpolation=cv2.INTER_AREA)
         self.basic_frame = cv2.cvtColor(self.basic_frame, cv2.COLOR_BGR2GRAY)
@@ -67,7 +49,6 @@ class Node:
                 self.prev_coords[0] = (x1, y1)
                 self.prev_coords[1] = (x2, y2)
             cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
->>>>>>> edbd127b8a02a424e5b2e4d20a488e92b5e1482f
 
         cv2.imshow('Face Detector', self.frame)
         cv2.waitKey(1)
