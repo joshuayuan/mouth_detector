7dbd472f7271e017b7cd76351d8d802e5c67d1db
Joshua Yuan <joshua43210@gmail.com>
2017-11-27 01:59:12 +0000

    starting point cloud stuff

--- src/detector_node.py
+++ src/detector_node.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import cv2
-import numpy as np
-import rospy
-import rospkg
-import detector
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
+import numpy as np
 
-class Node:
+import rospy, rospkg
+import message_filters, tf
+from sensor_msgs.msg import Image, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
 
-    SCALE_FACTOR = 0.5
+import detector
+
+class DetectorNode:
+
     def __init__(self):
         self.bridge = CvBridge()
         self.detector = detector.Detector()
-        # import pdb; pdb.set_trace()
+        self.broadcaster = tf.TransformBroadcaster()
+
         rospack = rospkg.RosPack()
         cascade_path = rospack.get_path('mouth_detector')+"/src/haarcascade_frontalface_default.xml"
         #cascade_path = rospy.get_param('/detector_node/cascade_path')
@@ -21,44 +24,31 @@ class Node:
         self.detector.loadCascade(cascade_path)
         if self.detector.hasEmptyCascade():
             raise IOError('Unable to load the face cascade classifier xml file')
-
+        #the following line is only for testing
         rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
+        image_sub = message_filters.Subscriber("/usb_cam/image_raw", Image) # Change to /kinect2/qhd/image_color_rect
+        point_cloud_sub = message_filters.Subscriber("/kinect2/qhd/points", PointCloud2)
 
-        self.basic_cascade = cv2.CascadeClassifier(cascade_path)
-        self.basic_frame = None
+        synch_sub = message_filters.TimeSynchronizer([image_sub, point_cloud_sub], 10)
+        synch_sub.registerCallback(self.callback)
+
         self.prev_coords = [(0, 0),(1, 1)] # (x, y) of top left
-        self.prev_dimensions = (0,0)
         self.loaded = False
 
-    def callback(self, image):
+    def callback(self, image, point_cloud=None):
         try:
             self.frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
             print(e)
+
         self.detector.loadFrameFindFace(self.frame)
-
-        #self.frame = cv2.resize(self.frame, None, fx=self.SCALE_FACTOR, fy=self.SCALE_FACTOR, interpolation=cv2.INTER_AREA)
-        #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        #basic_faces = self.basic_cascade.detectMultiScale(self.frame, 1.2, 3)
-        #if len(basic_faces) != 0:
-        #    x1, y1, x2, y2 = basic_faces[0]
-        #    cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
         if self.detector.faceIsFound():
             x1, y1, x2, y2 = self.detector.getFaceBox()
             px1, py1 = self.prev_coords[0]
             px2, py2 = self.prev_coords[1]
-            print("-", x1, y1, x2, y2)
-            print("P", px1, py1, px2, py2)
-            if self.loaded and ( abs(x1-px1) > self.frame.shape[0] / 8 or abs(y1-py1) / 8 > self.frame.shape[1]):
-                x1, y1 = self.prev_coords[0]
-                x2, y2 = self.prev_coords[1]
-            elif self.loaded:
-                self.prev_coords[0] = (x1, y1)
-                self.prev_coords[1] = (x2, y2)
-            if self.loaded and ((x2-x1)/2 > (px2-px1) or (y2-y1)/2 > (py2-py1) or (x2-x1)*2 < (px2-px1) or (y2-y1)*2 < (py2-py1)):
-                #print((x2-x1)/2 > (px2-px1) or (y2-y1)/2 > (py2-py1) or (x2-x1)*2 < (px2-px1) or (y2-y1)*2 < (py2-py1))
-                cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+            print("curr: ", x1, y1, x2, y2)
+            print("prev: ", px1, py1, px2, py2)
+            if self.loaded and (abs(x1-px1) > self.frame.shape[0] / 8 or abs(y1-py1) / 8 > self.frame.shape[1] or (x2-x1) / 2 > (px2-px1) or (y2-y1) / 2 > (py2-py1) or (x2-x1) * 2 < (px2-px1) or (y2-y1) * 2 < (py2-py1)):
                 x1, y1 = self.prev_coords[0]
                 x2, y2 = self.prev_coords[1]
             else:
@@ -75,9 +65,12 @@ class Node:
             self.loaded = False
         elif key == ord('q'):
             rospy.signal_shutdown("q was pressed")
+    
+    def transform_broadcast(self, x, y, z, timestamp):
+        self.broadcaster.sendTransform((x, y, z), tf.transformations.quaternion_from_euler(0, 0, 0), timestamp, "face", "kinect")
 
 
 if __name__ == '__main__':
-    node = Node()
-    rospy.init_node('node')
+    node = DetectorNode()
+    rospy.init_node('detector_node')
     rospy.spin()
