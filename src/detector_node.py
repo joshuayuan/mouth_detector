9f3690674c18535be4e22c66f81b920a02eaded8
Joshua Yuan <joshua43210@gmail.com>
2017-11-27 04:15:43 +0000

    comments

--- src/detector_node.py
+++ src/detector_node.py
@@ -19,13 +19,13 @@ class DetectorNode:
 
         rospack = rospkg.RosPack()
         cascade_path = rospack.get_path('mouth_detector')+"/src/haarcascade_frontalface_default.xml"
-        #cascade_path = rospy.get_param('/detector_node/cascade_path')
         print(cascade_path)
         self.detector.loadCascade(cascade_path)
         if self.detector.hasEmptyCascade():
             raise IOError('Unable to load the face cascade classifier xml file')
-        #the following line is only for testing
-        image_sub = message_filters.Subscriber("/kinect2/qhd/image_color_rect", Image) # Change to /kinect2/qhd/image_color_rect
+
+        # rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
+        image_sub = message_filters.Subscriber("/kinect2/qhd/image_color_rect", Image) 
         point_cloud_sub = message_filters.Subscriber("/kinect2/qhd/points", PointCloud2)
 
         synch_sub = message_filters.TimeSynchronizer([image_sub, point_cloud_sub], 10)
@@ -33,6 +33,7 @@ class DetectorNode:
 
         self.prev_coords = [(0, 0),(1, 1)] # (x, y) of top left
         self.loaded = False
+        self.debug = False
 
     def callback(self, image, point_cloud=None):
         try:
@@ -45,17 +46,20 @@ class DetectorNode:
             x1, y1, x2, y2 = self.detector.getFaceBox()
             px1, py1 = self.prev_coords[0]
             px2, py2 = self.prev_coords[1]
-            print("curr: ", x1, y1, x2, y2)
-            print("prev: ", px1, py1, px2, py2)
+            if self.debug:
+                print("curr: ", x1, y1, x2, y2)
+                print("prev: ", px1, py1, px2, py2)
             if self.loaded and (abs(x1-px1) > self.frame.shape[0] / 8 or abs(y1-py1) / 8 > self.frame.shape[1] or (x2-x1) / 2 > (px2-px1) or (y2-y1) / 2 > (py2-py1) or (x2-x1) * 2 < (px2-px1) or (y2-y1) * 2 < (py2-py1)):
+                # If the face box changes sizes too quickly or jumps too much, compared to the previous box, then we keep the previous box.
                 x1, y1 = self.prev_coords[0]
                 x2, y2 = self.prev_coords[1]
             else:
                 self.prev_coords[0] = (x1, y1)
                 self.prev_coords[1] = (x2, y2)
             cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
-            cv2.circle(self.frame, ((x1+x2)/2, y1/5+4*y2/5), (x2-x1)/6, (0, 0, 255), 2)
-            mouth_x, mouth_y = ((x1+x2)/2, y1/5+4*y2/5)
+
+            mouth_x, mouth_y = ((x1+x2)/2, y1/5+4*y2/5) # Estimated position of mouth
+            cv2.circle(self.frame, (mouth_x, mouth_y), (x2-x1)/6, (0, 0, 255), 2)
 
             cloud_out = pc2.read_points(point_cloud, ("x", "y", "z"), True, [(mouth_x, mouth_y)])
             try:
@@ -71,10 +75,13 @@ class DetectorNode:
             self.loaded = True
         elif key == ord('u'):
             self.loaded = False
+        elif key == ord('d'):
+            self.debug = True
         elif key == ord('q'):
             rospy.signal_shutdown("q was pressed")
 
     def transform_broadcast(self, x, y, z, timestamp, source_frame):
+        # sends out tf
         self.broadcaster.sendTransform((x, y, z), tf.transformations.quaternion_from_euler(0, 0, 0), timestamp, "face", source_frame)
 
 
