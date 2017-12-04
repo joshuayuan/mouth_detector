260208e3f19d7d0247f06a677d203bb14c375645
Joshua Yuan <joshua43210@gmail.com>
2017-12-03 19:28:24 -0800

    update to brent

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -4,14 +4,10 @@ import time
 import rospy
 from mouth_detector.srv import TriggerPhase
 
-brent_site = "https://brentyi.com/marshmellow"
+brent_site = "https://brentyi.com/marshmallow/phase"
+most_recent_phase = 0
 
-def serviceCall():
-    f = urllib2.urlopen(brent_site)
-    page_source = f.read()
-    value = parse(page_source)
-
-
+def serviceCall(value):
     try:
         phase = rospy.ServiceProxy('move_robot', TriggerPhase)
         response = phase(int(value))
@@ -26,8 +22,16 @@ def parse(source):
 
 def loop():
     rospy.wait_for_service('move_robot')
-    while True:
-        serviceCall()
-        time.sleep(1)
+
+    value = 0
+    while most_recent_phase !=  value:
+        f = urllib2.urlopen(brent_site)
+        page_source = f.read()
+
+        value = int(parse(page_source))
+        most_recent_phase = value
+
+        serviceCall(value)
+        time.sleep(1000)
 
 loop()
